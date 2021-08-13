1ad9e54cf50abab07aa42d1fd4e109402755ad54
anmolarora1711 <anmol1610140@akgec.ac.in>
2021-08-13 14:28:30 +0530

    Adding layout and controller for form submission

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form');
+    res.render('form', {title: 'Registration form'});
 });
+
+router.post('/', (req, res) => {
+    res.render('form', {title: 'Registration fomr'});
+})
 
 module.exports = router;
